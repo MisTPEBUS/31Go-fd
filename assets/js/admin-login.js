ef64a3d0aa745ba9eed5352b3bcd78dc98f57940
Lobinda <lobinda123@gmail.com>
2026-06-10 11:55:06 +0800

    add API_URL

--- assets/js/admin-login.js
+++ assets/js/admin-login.js
@@ -1,5 +1,5 @@
-/* let API_BASE_URL =
-    "https://ab89-2001-b011-3-11e3-48c-6ba4-3f87-5184.ngrok-free.app"; */
+let API_BASE_URL =
+  "https://ab89-2001-b011-3-11e3-48c-6ba4-3f87-5184.ngrok-free.app";
 
 const sendCodeBtn = document.getElementById("sendCodeBtn");
 
